87409f7cf22e2eb0a031ad2bbc65838acbd679d8
minhal128 <rizvi4600779@cloud.neduet.edu.pk>
2026-09-15 21:20:56 +0500

    fix: polished sidebar and main UI

--- app.config.js
+++ app.config.js
@@ -3,10 +3,5 @@ import 'dotenv/config';
 export default ({ config }) => {
     return {
         ...config,
-        extra: {
-            eas: {
-                projectId: "558bfea9-b275-4b59-8099-3ef01956b1ba",
-            },
-        },
     };
 };
